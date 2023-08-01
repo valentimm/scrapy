0e56db63ca42edea734d5419a6812080b866cdad
Matheus Valentim <meduardo.valentim@gmail.com>
2023-08-01 18:43:01 -0300

    feat: started first pipeline

---
+++ bookscraper/bookscraper/pipelines.py
@@ -0,0 +1,21 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class BookscraperPipeline:
+    def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+        
+        return item
